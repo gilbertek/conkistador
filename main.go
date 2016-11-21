82f804b2a65ca1a7e7d90751942b438cc7bf73e1
Gilbert F. Sewovoe-Ekoue <gsewovoe-ekoue@teladoc.com>
2016-11-21 14:15:07 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+type Location struct {
+	Description string
+	Transitions []string
+	Events      []string
+}
+
+type Event struct {
+	Type        string
+	Chance      int
+	Description string
+	Health      int
+	Evt         string
+}
+
+var events = map[string]*Event{
+	"alienAttack": {
+		Chance:      20,
+		Description: "An alien beams in front of you and shoots you with a ray gun.",
+		Health:      -50, Evt: "doctorTreatment"},
+	"doctorTreatment": {
+		Chance:      10,
+		Description: "The doctor rushes in and inject you with a health boost.",
+		Health:      +30, Evt: ""},
+	"android": {
+		Chance:      50,
+		Description: "Data is in the turbo lift and says hi to you",
+		Health:      0, Evt: ""},
+	"relaxing": {
+		Chance:      100,
+		Description: "In the lounge you are so relaxed that your health improves.",
+		Health:      +10, Evt: ""},
+}
+
+var locationMap = map[string]*Location{
+	"Bridge": {
+		"You are on the bridge of a spaceship sitting in the Captain's chair.",
+		[]string{"Ready Room", "Turbo Lift"},
+		[]string{"alienAttack"}},
+	"Ready Room": {
+		"The Captain's ready room.",
+		[]string{"Bridge"},
+		[]string{}},
+	"Turbo Lift": {
+		"A Turbo Lift that takes you anywhere in the ship.",
+		[]string{"Bridge", "Lounge", "Engineering"},
+		[]string{"android"}},
+	"Engineering": {"You are in engineering where you see the star drive",
+		[]string{"Turbo Lift"}, []string{"alienAttack"}},
+	"Lounge": {"You are in the lounge, you feel very relaxed",
+		[]string{"Turbo Lift"}, []string{"relaxing"}},
+}
+
+func main() {
+
+}
